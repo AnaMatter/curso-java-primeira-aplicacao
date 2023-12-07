991df8d458db26ab3d497dd3472542100fe08025
Ana <123168072+AnaMatter@users.noreply.github.com>
2023-12-07 09:31:27 -0300

    Delete IdeaProjects directory

--- IdeaProjects/alura/src/For.java
+++
@@ -1,8 +0,0 @@
-public class For {
-    public static void main(String[] args) {
-
-        for (int contador = 1; contador <= 10 ; contador++) {
-            System.out.println(contador);
-        }
-    }
-}
